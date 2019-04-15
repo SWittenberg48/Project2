182603c665447b95dd09bad912387c8bc4608753
kjoneil <kjoneil.rw@gmail.com>
2019-04-14 19:29:18 -0500

    Set up trip.html still need to create javascript for page

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,3 @@
 CREATE DATABASE city_trippin_db;
 
 use city_trippin_db;
-
-create table landmarks_chicago(
-id integer(12) auto_increment not null,
-landmark varchar(100),
-phone varchar(14),
-address varchar(100),
-rating varchar(5),
-hours varchar(100),
-info varchar(500),
-primary key (id)
-);
-
-select * from landmarks_chicago;
